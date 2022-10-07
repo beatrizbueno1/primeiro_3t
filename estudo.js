6214b412b3fd8ba7a47a695dbf22c3d2e27680c9
anaclara1301 <110241554+anaclara1301@users.noreply.github.com>
2022-10-07 11:10:40 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,36 @@
 
+let tabuada = 2;
+let nome= "ana clara"
+
+ function escrever(){
+    document.write(tabuada +" x 1 ="+(tabuada*1)+"<br>");
+    document.write(tabuada +" x 1 ="+(tabuada*1)+"<br>");
+    document.write(tabuada +" x 1 ="+(tabuada*1)+"<br>");
+    document.write(tabuada +" x 1 ="+(tabuada*1)+"<br>");
+    document.write(tabuada +" x 1 ="+(tabuada*1)+"<br>");
+    document.write(tabuada +" x 1 ="+(tabuada*1)+"<br>");
+    document.write(tabuada +" x 1 ="+(tabuada*1)+"<br>");
+    document.write(tabuada +" x 1 ="+(tabuada*1)+"<br>");
+    document.write(tabuada +" x 1 ="+(tabuada*1)+"<br>");
+    document.write(tabuada +" x 1 ="+(tabuada*1)+"<br>");
+    document.write("feito por" + nome)
+
+
+
+    function minhaTabuada(){
+        for(let i = 1; j <= 9; i++){
+        document.write(" tabuada do " +i + " <br> ");
+        for(let j = 1; j <=10; j++){
+            document.write(i + " x " + " = "(j*j)+ " <br> ");
+        }
+        document.write("<br>")
+        }
+
+
+        function quadrado(){
+            for(let i = 16; i <= 20; i++){
+                document.write("0 quadrado de " + i + " é " + (i*i) "<br>")
+            }
+        }
+    }
+ }
